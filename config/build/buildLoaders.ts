d4fc41bf93254c53aadd5ee60b3bcdea6f29d8aa
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-01-10 15:21:17 +0300

    add css.modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,5 +1,29 @@
 import webpack from "webpack";
-export function buildLoaders(): webpack.RuleSetRule[]{
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[]{
+
+    const cssLoader = {
+            test: /\.s[ac]ss$/i,
+            use: [
+                isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                {
+                    loader: "css-loader",
+                    options: {
+                        modules: {
+                            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                            localIdentName: isDev
+                                ? '[path].[name]__[local]--[hash:base64:5]'
+                                : '[hash:base64:8]'
+                        },
+
+                    },
+                },
+                "sass-loader",
+            ],
+        }
+
+
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -8,5 +32,6 @@ export function buildLoaders(): webpack.RuleSetRule[]{
 
     return [
         typescriptLoader,
+        cssLoader,
     ]
 }
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-export function someFn(arg: number): string {
-    console.log('fdgvdfg fegdfg sfgsg')
-    return 'че за хуйня?'
-}
