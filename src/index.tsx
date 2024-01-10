7396ef587a3760c6ee239096a3422efecfa3f101
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-01-10 23:51:40 +0300

    fix ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import {render} from "react-dom";
 import React from "react";
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
+import ThemProvider from "./theme/ThemProvider";
 
 render(
     <BrowserRouter>
-        <App/>
+        <ThemProvider>
+            <App/>
+        </ThemProvider>
     </BrowserRouter>,
     document.getElementById('root')
 )
