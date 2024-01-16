9b4f79622af4fd4af6bd286d1082973cb0723391
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-01-16 12:13:56 +0300

    add jest,add test classNames

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,12 +1,12 @@
+type Mods = Record<string, boolean | string>;
 
+export function classNames(cls: string, mods?: Mods, additional?: string[]): string {
+    const additionalClasses = additional ? additional.filter(Boolean) : [];
+    const modClasses = mods
+        ? Object.entries(mods)
+            .filter(([className, value]) => Boolean(value))
+            .map(([className]) => className)
+        : [];
 
-type Mods = Record<string, boolean | string>
-
-
-
-
-
-export function classNames(cls: string, mods?: Mods, additional?: string[] ): string {
-    return [cls, ...additional.filter(Boolean),...Object.entries(mods).filter(([className, value])=> Boolean(value)).map(([className,value]) => className)].join(' ')
+    return [cls, ...additionalClasses, ...modClasses].join(' ');
 }
-
