daae2eaf2cd8876deb3cb63f552dacabf1398a8e
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-01-26 20:32:46 +0300

    storybook add Decorator

--- src/shared/config/storybook/StyleDecorator/StyleDecorator.tsx
+++ src/shared/config/storybook/StyleDecorator/StyleDecorator.tsx
@@ -1,5 +1,6 @@
 // StyleDecorator.ts
-import React from 'react';
-import {Story, StoryContext, StoryFn} from '@storybook/react';
+
+import {Story} from '@storybook/react';
 import 'app/styles/index.scss'
 export const StyleDecorator = (story: () => Story) => story();
+//fg
