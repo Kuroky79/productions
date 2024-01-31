9513f3351ba750ac2c6d636bb818b7eec7ea82bc
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-01-31 12:46:30 +0300

    style button sidebar

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -3,17 +3,25 @@ import cls from './Button.module.scss'
 import React, {ButtonHTMLAttributes, FC} from "react";
 export enum ThemButton{
     CLEAR = 'clear',
-    OUTLINE = 'outline'
+    OUTLINE = 'outline',
+    BACKGROUND = 'background',
+    BACKGROUND_INVERTED = 'backgroundInverted'
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
     className?: string;
     them?: ThemButton;
+    square?: boolean;
 }
 
-export const Button: FC<ButtonProps> = ({ className, children, them, ...otherProps }) => {
+export const Button: FC<ButtonProps> = ({ className, children, them,square, ...otherProps }) => {
+
+    const mods = {
+        [cls.square]: square
+    }
+
     return (
-        <button className={classNames(cls.Button, {}, [className, cls[them]])} {...otherProps}>
+        <button className={classNames(cls.Button, mods, [className, cls[them]])} {...otherProps}>
             {children}
         </button>
     );
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,8 @@ import cls from './Sidebar.module.scss'
 import React, {useState} from "react";
 import ThemSwither from "widgets/ThemSwither/ui/ThemSwither";
 import LangSwither from "widgets/LangSwither/ui/LangSwither";
+import Button, {ThemButton} from "shared/ui/Button/Button";
+
 interface SidebarProps {
     className?: string;
 }
@@ -17,7 +19,13 @@ export const Sidebar = ({className}: SidebarProps) => {
 
     return (
         <div data-testid="sidebar" className={classNames(cls.Sidebar,{[cls.collapsed]: collapsed},[className])}>
-            <button data-testid="sidebar-toggle" onClick={onToggle}>toSide</button>
+            <Button
+                data-testid="sidebar-toggle"
+                className={cls.collapseBtn}
+                onClick={onToggle}
+                them={ThemButton.BACKGROUND_INVERTED}
+                square
+            >{collapsed ? '>' : '<'}</Button>
             <div className={cls.swithers}>
                 <ThemSwither/>
                 <LangSwither className={cls.lang}/>
