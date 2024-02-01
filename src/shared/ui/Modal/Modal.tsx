7f1e5c0942b41d8ecb4aec3e36e51fb1ed09c8fb
Alabuzhev Daniil <90332154+Kuroky79@users.noreply.github.com>
2024-02-01 22:24:23 +0300

    add change Modal close

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from './Modal.module.scss';
-import { ReactNode } from "react";
+import {ReactNode, useRef, useState} from "react";
 import React from "react";
 interface ModalProps {
     className?: string;
@@ -10,18 +10,23 @@ interface ModalProps {
 }
 
 
-
+const ANIMATION_DELAY = 300;
 
 export const Modal = ({ className, children,isOpen,onClose }: ModalProps) => {
     const mods: Record<string,boolean> = {
         [cls.opened]: isOpen,
     }
+
     const closeHandler = () =>{
         if(onClose){
             onClose()
+            timerRef.current = setTimeout(()=>{
+                onClose();
+            },ANIMATION_DELAY)
         }
     }
-
+    const [isClosing, setIsClosing] = useState(false);
+    const timerRef = useRef<NodeJS.Timeout>();
     const contentClick = (e: React.MouseEvent) => {
         e.stopPropagation();
     }
